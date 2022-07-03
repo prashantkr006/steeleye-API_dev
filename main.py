b39fce5e2c4054c211450453d37699d505a49ec6
prashantkr006 <prashantkr006@gmail.com>
2022-07-03 21:30:55 +0530

    created some end points

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import models
 import schemas
 from database import engine, SessionLocal
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
 
 app = FastAPI()
 
@@ -26,48 +26,35 @@ async def get_trades(db: Session = Depends(get_db), limit: Optional[int] = None)
     return db.query(models.Trade).limit(limit).all()
 
 
-# route to return single trade by id
-# @app.get("/trades/{trade_id}")
-# async def single_trade(id):
-#     return "single trade by id"
+@app.get("/trades/{trade_id}")
+async def get_single_trade(trade_id: str, db: Session = Depends(get_db)):
+    return db.query(models.Trade).filter(models.Trade.trade_id == trade_id).first()
 
 
-# route to return trades based on searched query
-# @app.get("/trades/")
-# async def search_trades(counterparty, instrumentId, instrumentName, trader):
-#     return "trades according to search values"
-
-
-# route to create new trades
-# @app.post("/trade_details")
-# async def create_trade_details(req: schemas.TradeDetails):
-#     return "creating trade details"
+# @app.get("/search")
+# async def search_trades(counterparty: str, instrumentId: str, instrumentName: str, trader: str,
+#                         db: Session = Depends(get_db)):
+#     return db.query(models.Trade).filter(
+#         models.Trade.counterparty == counterparty and models.Trade.instrument_id == instrumentId and
+#         models.Trade.instrument_name == instrumentName and models.Trade.trader == trader)
 
 
 # route to create trade
 @app.post("/trade")
 async def create_trade(trades: schemas.Trade, db: Session = Depends(get_db)):
+    new_trade_Details = models.TradeDetails(price=trades.trade_details.price,
+                                            buySellIndicator=trades.trade_details.buySellIndicator,
+                                            quantity=trades.trade_details.quantity)
+    db.add(new_trade_Details)
+    db.commit()
+    db.refresh(new_trade_Details)
+
     new_trade = models.Trade(trade_id=trades.trade_id, trader=trades.trader, asset_class=trades.asset_class,
+                             trade_details_id=new_trade_Details.id,
                              counterparty=trades.counterparty, trade_date_time=trades.trade_date_time,
                              instrument_id=trades.instrument_id, instrument_name=trades.instrument_name)
     db.add(new_trade)
     db.commit()
     db.refresh(new_trade)
 
-    return new_trade
-    # new_trade_Details = models.TradeDetails(price=req.trade_details.price,
-    #                                         buySellIndicator=req.trade_details.buySellIndicator,
-    #                                         quantity=req.trade_details.quantity)
-    # db.add(new_trade_Details)
-    # db.commit()
-    # db.refresh(new_trade_Details)
-    # new_trade = models.Trade(trade_id=req.trade_id, trader=req.trader, asset_class=req.asset_class,
-    #                          counterparty=req.counterparty,
-    #                          trade_details_id=new_trade_Details.id,
-    #                          instrument_id=req.instrument_id
-    #                          , instrument_name=req.instrument_name, trade_date_time=req.trade_date_time)
-    # db.add(new_trade)
-    # db.commit()
-    # db.refresh(new_trade)
-    # data = db.get(models.Trade, new_trade.trade_id)
-    # return data
+    return "trade added successfully"
